2bf416d1b70912fa293b88eb30cd309c13008941
Utkarsh Verma <utkar.sh@outlook.com>
2017-08-29 01:13:15 +0530

    Maps Added

--- swachhathon/src/pages/home/home.ts
+++ swachhathon/src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+declare var google;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,28 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ionViewDidLoad(){
+    this.loadMap();
+  }
+
+  loadMap(){
+
+    let latLang = new google.maps.LatLng(-34.9290, 138.6010);
+
+    let mapOptions = {
+      center: latLang,
+      zoom: 15,
+      myTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
   }
 
 }

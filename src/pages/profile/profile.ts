885ca166658e6434d10bf061762065ef2f148406
Nikhil Mishra <nkmishra1997@gmail.com>
2017-09-07 18:42:12 +0530

    Hopefully Final

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { MapPage } from '../map/map';
 import { AdminPagePage } from '../admin/admin';
+import { AlertController } from 'ionic-angular';
 
 import md5 from 'crypto-md5';
  
@@ -15,13 +16,28 @@ export class Profile {
     password: any;
     profilePicture: any = "https://www.gravatar.com/avatar/"
  
-    constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController, private alertCtrl: AlertController) {
  
     }
+
+    ionViewDidLoad() {
+        console.log('Hello profile Page');
+        this.presentAlert();
+      }
  
     emailChanged(){
         this.profilePicture = "https://www.gravatar.com/avatar/" + md5(this.email.toLowerCase(), 'hex');
     }
+
+    presentAlert() {
+        let alert = this.alertCtrl.create({
+          title: 'Do you Know ',
+          subTitle: 'Itne log mar gaye  ',
+          buttons: ['Ok']
+        });
+        alert.present();
+      } 
+    
 
     goToMapPage(){
         this.navCtrl.push(MapPage);
